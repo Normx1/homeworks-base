e870a8f1f7014303b81b448c4d906914a4d8e1da
Сергей <Normande2010@mail.ru>
2020-11-10 23:56:42 +0300

    Без тестов

--- src/main/java/homework3/Task1.java
+++ src/main/java/homework3/Task1.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class Task1 {
 
     public static void main(String[] args) {
-        System.out.println("Введите размерность матрицы");
+        System.out.println("Введите размерность матрицы: ");
         Scanner sc = new Scanner(System.in);
         Random r = new Random();
         int n;
@@ -12,12 +12,14 @@ public class Task1 {
         int pr2 = 1;
 
         int mass[][] = new int[n][n];
+        int mass2[][] = new int[n][n];
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 mass[i][j] = r.nextInt(50);
             }
         }
+        System.out.println("Матрица: ");
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 System.out.print(mass[i][j] + " ");
@@ -31,42 +33,58 @@ public class Task1 {
                 summ += mass[i][j];
             }
         }
-//        System.out.println("Сумма главной диагонали- " + summ);
-//        System.out.println("Нечетные элем под гл. диагональю(вкл): ");
-//        for (int i = 0; i < n; i++) {
-//            for (int j = 0; j <= i; j++) {
-//                if ((mass[i][j] % 2) == 1) {
-//                    System.out.print(mass[i][j] + " ");
-//                }
-//            }
-//        }
-//        System.out.println();
-//
-//        for (int i = 0; i < n; i++) {
-//            int j = i;
-//            pr1 = mass[i][j]*pr1;
-//                }
-//        for (int i= 0; i < n; i++){
-//                int j= n-i-1;
-//                pr2 = mass[i][j] * pr2;
-//        }
-//        System.out.println(pr1);
-//        System.out.println(pr2);
-//        if (pr1 > pr2){
-//            System.out.println("Произведение 1-й диагонали - больше");
-//        }
-//        if (pr1 < pr2){
-//            System.out.println("Произведение 2-й диагонали - больше");
-//        }
-//        if (pr1== pr2){
-//            System.out.println(" Произведения диагоналей - равны");
-//        }
-        System.out.println("Четные элем над поб. диагональю(не вкл): ");
-        for (int i = 0; i <n; i++) {
-            for (int j = n-i-1; j>=0; j--)
-            if (n-i!=j+1 & (mass[i][j] % 2) == 0) {
-                System.out.print(mass[i][j] + " ");
-           }
+        System.out.println("Сумма эл. главной диагонали- " + summ);
+        System.out.println("Нечетные элем под гл. диагональю(вкл): ");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                if ((mass[i][j] % 2) == 1) {
+                    System.out.print(mass[i][j] + " ");
+                }
+            }
         }
-    }
+        System.out.println();
+
+        for (int i = 0; i < n; i++) {
+            int j = i;
+            pr1 = mass[i][j] * pr1;
+        }
+        for (int i = 0; i < n; i++) {
+            int j = n - i - 1;
+            pr2 = mass[i][j] * pr2;
+        }
+        System.out.println("Произведение элементов гл. диагонали:" + "\n" + pr1);
+        System.out.println("Произведение элементов втор. диагонали:" + "\n" + pr2);
+        if (pr1 > pr2) {
+            System.out.println("Произведение 1-й диагонали - больше");
+        }
+        if (pr1 < pr2) {
+            System.out.println("Произведение 2-й диагонали - больше");
+        }
+        if (pr1 == pr2) {
+            System.out.println(" Произведения диагоналей - равны");
+        }
+        System.out.println("Четные элем над поб. диагональю(не вкл): ");
+        for (int i = 0; i < n; i++) {
+            for (int j = n - i - 1; j >= 0; j--) {
+                if (n - i != j + 1 & (mass[i][j] % 2) == 0) {
+                    System.out.print(mass[i][j] + " ");
+                }
+            }
+        }
+
+            System.out.println("\n" + "Транспонированная матрица: ");
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    mass2[i][j] = mass[j][i];
+                }
+            }
+
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    mass[i][j] = mass2[i][j];
+                    System.out.print(mass[i][j] + " ");
+                }
+                System.out.println();
+            }
+        }
 }
