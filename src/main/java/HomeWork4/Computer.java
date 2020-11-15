dc8856731d3e8d8dc76007ae2c85dca181496017
Сергей <Normande2010@mail.ru>
2020-11-15 17:03:29 +0300

    Добавлен метод выключения

--- src/main/java/HomeWork4/Computer.java
+++ src/main/java/HomeWork4/Computer.java
@@ -9,25 +9,56 @@ public class Computer {
     int hardDrive;
     Boolean staus;
 
-// Метод описания
-      static void dyspalay(String a){
+    // Метод описания
+    static void dyspalay(String a) {
         System.out.println(a);
     }
 
-    static  void Vkl(int b){
+    static void Vkl(int b) {
         int rand = 0;
         int vvod = 0;
-        System.out.println("Включить компьютер");
-        Random r = new Random();
-        Scanner sc = new Scanner(System.in);
-        rand = r.nextInt(1);
-        vvod = sc.nextInt();
-        if(rand == vvod){
-            System.out.println("Угадал!.Компьютер всего-то выключился!");
-        }else{
-            System.out.println("Не угадал, компьютер- сгорел");
+        String vkl;
+        System.out.println("Включить компьютер Yes/No?");
+        Scanner vklsc = new Scanner(System.in);
+        vkl = vklsc.nextLine();
+        if (vkl == "Yes") {
+            System.out.println("Упс- сбой... Необходимо ввести число 1 или 0, чтобы спасти комп!");
+            Random r = new Random();
+            Scanner sc = new Scanner(System.in);
+            rand = r.nextInt(2);
+            vvod = sc.nextInt();
+            if (rand == vvod) {
+                System.out.println("Угадал! Компьютер всего-то выключился!");
+            } else {
+                System.out.println("Не угадал, компьютер- сгорел");
+            }
+        } else {
+            System.out.println("Компьютер выключен");
         }
 
+    }
+
+    static void Close(int b) {
+        int rand = 0;
+        int vvod = 0;
+        String off;
+        System.out.println("Выключить компьютер Yes/No?");
+        Scanner vklsc = new Scanner(System.in);
+        off = vklsc.nextLine();
+        if (off == "Yes") {
+            System.out.println("Упс- сбой... Необходимо ввести число 1 или 0, чтобы спасти комп!");
+            Random r = new Random();
+            Scanner sc = new Scanner(System.in);
+            rand = r.nextInt(2);
+            vvod = sc.nextInt();
+            if (rand == vvod) {
+                System.out.println("Угадал! Компьютер всего-то выключился!");
+            } else {
+                System.out.println("Не угадал, компьютер- сгорел");
+            }
+        } else {
+            System.out.println("Компьютер продолжает работать");
+        }
     }
 }
 
