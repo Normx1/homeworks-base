3b4b14a5fae8b46ea0ccc59297f51d6ed9cd0449
Сергей <Normande2010@mail.ru>
2020-11-19 00:31:44 +0300

    Без проверки

--- src/main/java/HomeWork5/LandTransport.java
+++ src/main/java/HomeWork5/LandTransport.java
@@ -3,23 +3,10 @@ package HomeWork5;
 public class LandTransport extends Transport {
     public int numOfWeels;
     public int fuelCons;
-    public LandTransport( int numOfWeels, int fuelCons){
-        super();
+
+    public LandTransport(int power, int maxSpeed, int weight, String model, int numOfWeels, int fuelCons) {
+        super(power, maxSpeed, weight, model);
         this.numOfWeels = numOfWeels;
         this.fuelCons = fuelCons;
-    }
-
-//    public LandTransport(int numOfWeels, int fuelCons) {
-//        this.numOfWeels = numOfWeels;
-//        this.fuelCons = fuelCons;
-//    }
-
-    public void dispalayInfo(){
-        System.out.printf("Мощность: %s\nМаксимальная скорость: %s\nЧисло колес: %s\n ",power, maxSpeed, numOfWeels);
-    }
-    public static void main(String[] args){
-//        Transport transport = new Transport(12,20,5000,"bmw");
-        LandTransport Car = new LandTransport(4,50);
-        Car.dispalayInfo();
     }
 }
