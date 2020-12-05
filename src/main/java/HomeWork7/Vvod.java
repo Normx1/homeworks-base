c1915cd0b676502aa3c2abeb55b745296d60ef60
Сергей <Normande2010@mail.ru>
2020-12-05 12:59:11 +0300

    Ввод

---
+++ src/main/java/HomeWork7/Vvod.java
@@ -0,0 +1,35 @@
+package HomeWork7;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Vvod {
+
+    public static ArrayList<String> Vvod(ArrayList a) {
+        ArrayList<String> arr1 = new ArrayList<>();
+        Scanner sc1 = new Scanner(System.in);
+        String per = sc1.nextLine();
+        String[] arr = per.split("/");
+        for (int i = 0; i < arr.length; i++) {
+            arr1.add(arr[i] + "/");
+        }
+        Boolean truth = true;
+        while (true) {
+            for (int i = 0; i < arr1.size() ; i++) {
+                if (truth != false) {
+                    truth = VyvodMass.PointSearch(arr1.get(i));
+                } else {
+                    System.out.println("Запрещено создавать в файле каталоги и другие файлы!");
+                    return a;
+                    // можно попробовать здесь вызвать ошибку
+                }
+            }
+
+
+            a.add(arr1);
+//            VyvodMass.VyvodArrArr(a);
+            break;
+        }return arr1;
+    }
+
+}
