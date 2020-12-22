33db283d3820b918ce0c20d195f5c8f0cd2a031b
Сергей <Normande2010@mail.ru>
2020-12-22 22:06:01 +0300

    dz1- Shop.01

--- src/main/java/HomeWork10/Shop.java
+++ src/main/java/HomeWork10/Shop.java
@@ -2,6 +2,7 @@ package HomeWork10;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 
 public class Shop {
     public Map productList;
@@ -24,7 +25,7 @@ public class Shop {
     }
 
     public static void removeProduct(Map shopList, Integer id) {
-        if (shopList.containsKey(id) == true) {
+        if (shopList.containsKey(id)) {
             shopList.remove(id);
             System.out.println("Удаление прошло успешно!");
         } else {
@@ -36,16 +37,69 @@ public class Shop {
         return (Map) shoplist.values();
     }
 
-    public static void showShopList(Map<Integer,Product> shopList) {
-        for (Map.Entry<Integer,Product>  entry : shopList.entrySet()) {
-            System.out.println(entry.getValue().getName());
+    public static void showShopList(Map<Integer, Product> shopList) {
+        for (Map.Entry<Integer, Product> newEntry : shopList.entrySet()) {
+            System.out.printf("id: %s; Name: %s; Price: %s$;", newEntry.getValue().getId(), newEntry.getValue().getName(),
+                    newEntry.getValue().getPrice());
+            System.out.println();
+
         }
     }
 
+    public static void editProduct(Map<Integer, Product> shopList) {
+        int i = 0;
+        while (i != 1) {
+            try {
+                System.out.println("Введите id товара: ");
+                Scanner sc1 = new Scanner(System.in);
+                Integer idProduct = sc1.nextInt();
+                Product editedProduct = shopList.get(idProduct);
+                System.out.printf("id: %s; Name: %s; Price: %s$;", editedProduct.getId(), editedProduct.getName(),
+                        editedProduct.getPrice());
+                System.out.println();
+                System.out.println("Введите новый id");
+                Scanner sc2 = new Scanner(System.in);
+                Integer newIdProduct = sc2.nextInt();
+                editedProduct.setId(newIdProduct);
+
+
+                System.out.println("Введите новое имя:");
+                Scanner sc3 = new Scanner(System.in);
+                String newName = sc3.nextLine();
+                editedProduct.setName(newName);
+
+                System.out.println("Введите новую цену:");
+                Scanner sc4 = new Scanner(System.in);
+                Integer newPrice = sc4.nextInt();
+                editedProduct.setPrice(newPrice);
+            } catch (NullPointerException e) {
+                System.out.println("Данного id не существует!");
+                continue;
+            }
+            i++;
+        }
+    }
+
+
     public static void main(String[] args) {
         Map magazine = shop();
-        Product item1 = new Product(0,"apple", 50);
+        Product item1 = new Product(0, "apple", 5);
+        Product item2 = new Product(1, "banana", 10);
+        Product item3 = new Product(3, "pear", 40);
+        Product item4 = new Product(5, "milk", 15);
+
+
         addProduct(magazine, item1);
+        addProduct(magazine, item2);
+        addProduct(magazine, item3);
+        addProduct(magazine, item4);
+
         showShopList(magazine);
-     }
+        removeProduct(magazine, 3);
+        showShopList(magazine);
+
+        editProduct(magazine);
+        showShopList(magazine);
+
+    }
 }
