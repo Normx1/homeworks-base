da02fc53ab4514f8a4fae3e9a51f1f25deff1326
Сергей <Normande2010@mail.ru>
2020-12-23 22:18:35 +0300

    dz1- shop в классе treeMap

--- src/main/java/HomeWork10/Shop.java
+++ src/main/java/HomeWork10/Shop.java
@@ -3,101 +3,57 @@ package HomeWork10;
 import java.util.*;
 
 public class Shop {
-    public Map productList;
+    public TreeSet<Product> productList;
 
-    public Shop(Map productList) {
+    public Shop(TreeSet<Product> productList) {
         this.productList = productList;
     }
 
-    public static Map<Integer, Product> shop(Comparator a) {
-        Comparator<Product> comp1 = new NameCorporator();
-        Map<Integer, Product> shopList = new TreeMap(comp1);
-        return shopList;
+    public void addNewItems(Product newItem) {
+        productList.add(newItem);
     }
 
-    public static void addProduct(Map shopList, Product newProduct) {
-        try {
-            shopList.putIfAbsent(newProduct.getId(), newProduct);
-        } catch (IllegalArgumentException e) {
-            System.out.println("Товар с таким id уже существует!");
-        }
+    public TreeSet<Product> getProductList() {
+        return productList;
     }
 
-    public static void removeProduct(Map shopList, Integer id) {
-        if (shopList.containsKey(id)) {
-            shopList.remove(id);
-            System.out.println("Удаление прошло успешно!");
-        } else {
-            System.out.println("Товара с данным id не существует");
-        }
+    public void setProductList(TreeSet<Product> productList) {
+        this.productList = productList;
     }
 
-    public static Map addAllItems(Map shoplist) {
-        return (Map) shoplist.values();
+    public TreeSet<Product> getAllItems() {
+        return productList;
     }
 
-    public static void showShopList(Map<Integer, Product> shopList) {
-        for (Map.Entry<Integer, Product> newEntry : shopList.entrySet()) {
-            System.out.printf("id: %s; Name: %s; Price: %s$;", newEntry.getValue().getId(), newEntry.getValue().getName(),
-                    newEntry.getValue().getPrice());
-            System.out.println();
-
-        }
-    }
-
-    public static void editProduct(Map<Integer, Product> shopList) {
-        int i = 0;
-        while (i != 1) {
-            try {
-                System.out.println("Введите id товара: ");
-                Scanner sc1 = new Scanner(System.in);
-                Integer idProduct = sc1.nextInt();
-                Product editedProduct = shopList.get(idProduct);
-                System.out.printf("id: %s; Name: %s; Price: %s$;", editedProduct.getId(), editedProduct.getName(),
-                        editedProduct.getPrice());
-                System.out.println();
-                System.out.println("Введите новый id");
-                Scanner sc2 = new Scanner(System.in);
-                Integer newIdProduct = sc2.nextInt();
-                editedProduct.setId(newIdProduct);
-
-
-                System.out.println("Введите новое имя:");
-                Scanner sc3 = new Scanner(System.in);
-                String newName = sc3.nextLine();
-                editedProduct.setName(newName);
-
-                System.out.println("Введите новую цену:");
-                Scanner sc4 = new Scanner(System.in);
-                Integer newPrice = sc4.nextInt();
-                editedProduct.setPrice(newPrice);
-            } catch (NullPointerException e) {
-                System.out.println("Данного id не существует!");
-                continue;
+    public void removeItem(Integer id) {
+        for (Product p : productList) {
+            if (p.getId() == id) {
+                productList.remove(p);
+                System.out.println("Продукт успешно удален!");
+                break;
+            } else {
+                System.out.println("Продукта с данным id не найдено!");
             }
-            i++;
+        }
+    }
+
+    public void editItem(Product p) {
+        for (Product a : productList) {
+            if (a.getId() == p.getId()) {
+                a.setPrice(p.getPrice());
+                a.setName(p.getName());
+                System.out.println("Продукт успешно изменен!");
+            } else {
+                System.out.println("Данного продукта не найдено!");
+                productList.add(p);
+            }
+        }
+    }
+
+    public void seeAllItems(){
+        for (Product a :productList) {
+            System.out.printf("id: %s; Name: %s; Price: %s;",a.getId(),a.getName(),a.getPrice());
+            System.out.println();
         }
     }
 }
-
-class NameCorporator implements Comparator<Product> {
-    @Override
-    public int compare(Product o1, Product o2) {
-        return o1.getName().compareTo(o2.getName());
-    }
-}
-
-class PriceComporator implements Comparator<Product> {
-    @Override
-    public int compare(Map.Entry<Integer,Product>) {
-        if (o1.getPrice() > o2.getPrice()) {
-            return 1;
-        }
-        if (o1.getPrice() < o2.getPrice()) {
-            return -1;
-        } else {
-            return 0;
-        }
-    }
-}
-
--- src/main/java/HomeWork10/app.java
+++ src/main/java/HomeWork10/app.java
@@ -1,33 +1,65 @@
 package HomeWork10;
 
-import com.sun.source.tree.Tree;
-
 import java.util.*;
-
-import static HomeWork10.Shop.*;
 
 public class app {
 
     public static void main(String[] args) {
-        Comparator<Product> comp = new PriceComporator();
-        Map magazine = shop(comp);
+        Comparator<Product> pcom = new PriceComporator();
+        TreeSet<Product> magazine = new TreeSet<>(pcom);
+        Shop wallMart = new Shop(magazine);
+//        wallMart.setProductList(magazine);
         Product item1 = new Product(0, "pizza", 5);
         Product item2 = new Product(1, "banana", 10);
         Product item3 = new Product(2, "bred", 2);
         Product item4 = new Product(3, "apple", 4);
         Product item5 = new Product(4, "pear", 8);
-        Product item6 = new Product(5, "milk", 5);
+        Product item6 = new Product(2, "milk", 5);
 
-        addProduct(magazine, item1);
-        addProduct(magazine, item2);
-        addProduct(magazine, item3);
-        addProduct(magazine, item4);
-        addProduct(magazine, item5);
-        addProduct(magazine, item6);
-        showShopList(magazine);
 
-        removeProduct(magazine, 3);
+        // Метод добавления
+        wallMart.addNewItems(item1);
+        wallMart.addNewItems(item2);
+        wallMart.addNewItems(item3);
+        wallMart.addNewItems(item4);
+        wallMart.addNewItems(item5);
+        wallMart.addNewItems(item6);
 
-        showShopList(magazine);
+        wallMart.seeAllItems();
+        wallMart.removeItem(3);
+
+
+        wallMart.editItem(item6);
+        wallMart.seeAllItems();
+
+        wallMart.getAllItems();
+
+
+
+
+
+
+    }
+
+    static class NameComporator implements Comparator<Product> {
+        @Override
+        public int compare(Product o1, Product o2) {
+            return o1.getName().compareTo(o2.getName());
+        }
+
+    }
+
+    static class PriceComporator implements Comparator<Product> {
+        @Override
+        public int compare(Product o1, Product o2) {
+            if (o1.getPrice() > o2.getPrice()) {
+                return 1;
+            }
+            if (o1.getPrice() < o2.getPrice()) {
+                return -1;
+            } else {
+                return 0;
+            }
+        }
     }
 }
